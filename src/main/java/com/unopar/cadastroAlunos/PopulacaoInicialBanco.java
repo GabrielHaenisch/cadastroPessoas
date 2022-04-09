58e31441c80c1a7b5bafc4c28c983556d5955956
Gabriel Haenisch Cavalcante <haenisch2002@gmail.com>
2022-04-09 13:43:45 -0300

    teste

---
+++ src/main/java/com/unopar/cadastroAlunos/PopulacaoInicialBanco.java
@@ -0,0 +1,35 @@
+package com.unopar.cadastroAlunos;
+
+import java.time.LocalDate;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.unopar.cadastroAlunos.rh.dominio.Pessoa;
+import com.unopar.cadastroAlunos.rh.dominio.PessoaRepositorio;
+
+@Component
+@Transactional
+public class PopulacaoInicialBanco implements CommandLineRunner{
+
+	@Autowired
+	private PessoaRepositorio pessoaRepo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Pessoa p1 = new Pessoa("Joao");
+		p1.setDataNascimento(LocalDate.of(1990, 4, 1));
+		p1.setEmail("joao@gmail.com");
+		Pessoa p2 = new Pessoa("Maria");
+		p1.setDataNascimento(LocalDate.of(1990, 1, 1));
+		p1.setEmail("maria@gmail.com");
+		
+		pessoaRepo.save(p1);
+		pessoaRepo.save(p2);
+	}
+	
+}
